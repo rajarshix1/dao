1411f6729b4f139de29adfbe2e674faac1d8c1c5
rajarshix1 <rajarshim5@gmail.com>
2022-09-19 17:23:00 +0530

    web3

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,20 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { contractABI } from '../abi'
 import { constants } from '../constants'
+var Contract = require('web3-eth-contract');
+
+// set provider for all later instances to use
 export default function Home() {
+  Contract.setProvider(constants.bscRPC)
+  
+  var contract = new Contract(contractABI, constants.contractAddress);
+  const func = async () =>{
+    contract.methods.deployer().call()
+  }
   return (
     <div className={styles.container}>
      <h1>8k DAO</h1>
+     <button onClick={func}>click</button>
     </div>
   )
 }
