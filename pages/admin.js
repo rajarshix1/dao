28eaad559bc2ead703fcbc58430742e464eaeefb
subham07-t <82716446+subham07-t@users.noreply.github.com>
2022-10-15 13:24:01 +0530

    random design for rajarshi

--- pages/admin.js
+++ pages/admin.js
@@ -1,24 +1,28 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import { contractABI } from '../abi'
-import { bscRPC, constants, contractAddress, defaultChainId, defaultChainId_hex } from '../constants'
-import Web3 from 'web3'
-import { useEffect, useState } from 'react'
-var Contract = require('web3-eth-contract');
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { contractABI } from "../abi";
+import {
+  bscRPC,
+  constants,
+  contractAddress,
+  defaultChainId,
+  defaultChainId_hex,
+} from "../constants";
+import Web3 from "web3";
+import { useEffect, useState } from "react";
+var Contract = require("web3-eth-contract");
 
 // set provider for all later instances to use
 export default function Home() {
-  const [walletAddress, setWalletAddress] = useState('')
-  const [desc, setDesc] = useState("")
-  const [addr, setAddr] = useState(null)
-  const [amount, setAmount] = useState(null)
-  const [admin, setAdmin] = useState(false)
+  const [walletAddress, setWalletAddress] = useState("");
+  const [desc, setDesc] = useState("");
+  const [addr, setAddr] = useState(null);
+  const [amount, setAmount] = useState(null);
+  const [admin, setAdmin] = useState(false);
   const connectWallet = async () => {
-  
     // Asking if metamask is already present or not
     if (window.ethereum) {
-  
       // res[0] for fetching a first wallet
       window.ethereum
         .request({ method: "eth_requestAccounts" })
@@ -27,46 +31,58 @@ export default function Home() {
       alert("install metamask extension!!");
     }
   };
-  
+
   // getbalance function for getting a balance in
   // a right format with help of ethers
-  
+
   // Function for getting handling all events
   const accountChangeHandler = (account) => {
     // Setting an address data
-    setWalletAddress(account)
+    setWalletAddress(account);
     // Setting a balance
   };
 
-  const isAdmin = async () =>{
-    Contract.setProvider(window.ethereum)
+  const isAdmin = async () => {
+    Contract.setProvider(window.ethereum);
     var contract = new Contract(contractABI, contractAddress);
-    if(walletAddress.length>1){
-        const check = await contract.methods.hasRole('0x0000000000000000000000000000000000000000000000000000000000000000', walletAddress).call()
-        setAdmin(check)
-        console.log(check);
+    if (walletAddress.length > 1) {
+      const check = await contract.methods
+        .hasRole(
+          "0x0000000000000000000000000000000000000000000000000000000000000000",
+          walletAddress
+        )
+        .call();
+      setAdmin(check);
+      console.log(check);
     }
-  }
-//   const createProposal = async () =>{
-//     Contract.setProvider(window.ethereum)
-    
-//     var contract = new Contract(contractABI, contractAddress);
-//     const create = await contract.methods.createProposal(desc, charity, amount).send({ from: walletAddress })
-//   }
-  const createStakeHolder = async () =>{
-    Contract.setProvider(window.ethereum)
+  };
+  //   const createProposal = async () =>{
+  //     Contract.setProvider(window.ethereum)
+
+  //     var contract = new Contract(contractABI, contractAddress);
+  //     const create = await contract.methods.createProposal(desc, charity, amount).send({ from: walletAddress })
+  //   }
+  const createStakeHolder = async () => {
+    Contract.setProvider(window.ethereum);
     var contract = new Contract(contractABI, contractAddress);
-    if(admin){
-        const create = await contract.methods.makeStakeholder(addr).send({ from: walletAddress })
+    if (admin) {
+      const create = await contract.methods
+        .makeStakeholder(addr)
+        .send({ from: walletAddress });
     }
-  }
-  const createContributor = async () =>{
-    Contract.setProvider(window.ethereum)
+  };
+  const createContributor = async () => {
+    Contract.setProvider(window.ethereum);
     var contract = new Contract(contractABI, contractAddress);
-    if(admin){
-        const create = await contract.methods.grantRole("0xf4bb84e1768dd286358a736d195a0b228621173f655a73b89bf8b191ed0ba07d",addr).send({ from: walletAddress })
+    if (admin) {
+      const create = await contract.methods
+        .grantRole(
+          "0xf4bb84e1768dd286358a736d195a0b228621173f655a73b89bf8b191ed0ba07d",
+          addr
+        )
+        .send({ from: walletAddress });
     }
-  }
+  };
   // const createStakeHolder = async () =>{
   //   if(admin){
   //       const create = await contract.methods.makeStakeholder(addr).send({ from: walletAddress })
@@ -78,18 +94,64 @@ export default function Home() {
   //   }
   // }
   useEffect(() => {
-    isAdmin()
-  }, [walletAddress])
-
-
+    isAdmin();
+  }, [walletAddress]);
 
   return (
     <div className={styles.container}>
-     <h1>{walletAddress}</h1>
-     <button onClick={connectWallet}>Connect Wallet</button><br/>
-     <input onChange={(e)=>setAddr(e.target.value)}  ></input>
-     <button onClick={createStakeHolder}>Make StakeHolder</button>
-     <button onClick={createContributor}>Make Contributor</button>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          alignItems: "center",
+          padding: "8px 16px 8px 16px",
+        }}
+      >
+        {walletAddress && (
+          <button className={styles.btn} onClick={connectWallet}>
+            {walletAddress.substring(0, 4) +
+              "...." +
+              walletAddress.substring(38, walletAddress.length)}
+          </button>
+        )}
+        {!walletAddress && (
+          <button className={styles.btn} onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
+      </div>
+      <div
+        style={{
+          background: "#F6F7FC",
+          padding: "24px",
+          width: "50%",
+        }}
+      >
+        <input
+          style={{
+            width: "100%",
+            padding: "12px",
+            border: "1px solid grey",
+            borderRadius: "10px",
+          }}
+          onChange={(e) => setAddr(e.target.value)}
+        ></input>
+        <div
+          style={{
+            marginTop: "18px",
+            display: "flex",
+            justifyContent: "center",
+            gap: "12px",
+          }}
+        >
+          <button className={styles.btn} onClick={createStakeHolder}>
+            Make StakeHolder
+          </button>
+          <button className={styles.btn} onClick={createContributor}>
+            Make Contributor
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,29 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import { contractABI } from '../abi'
-import { bscRPC, constants, contractAddress, defaultChainId, defaultChainId_hex } from '../constants'
-import Web3 from 'web3'
-import { useEffect, useState } from 'react'
-var Contract = require('web3-eth-contract');
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { contractABI } from "../abi";
+import {
+  bscRPC,
+  constants,
+  contractAddress,
+  defaultChainId,
+  defaultChainId_hex,
+} from "../constants";
+import Web3 from "web3";
+import { useEffect, useState } from "react";
+var Contract = require("web3-eth-contract");
 
 // set provider for all later instances to use
 export default function Home() {
-  const [walletAddress, setWalletAddress] = useState('')
-  const [desc, setDesc] = useState("")
-  const [charity, setCharity] = useState(null)
-  const [amount, setAmount] = useState(null)
-  const [allProposals, setAllProposals] = useState([])
- 
+  const [walletAddress, setWalletAddress] = useState("");
+  const [desc, setDesc] = useState("");
+  const [charity, setCharity] = useState(null);
+  const [amount, setAmount] = useState(null);
+  const [allProposals, setAllProposals] = useState([]);
+
   const connectWallet = async () => {
-  
     // Asking if metamask is already present or not
     if (window.ethereum) {
-  
       // res[0] for fetching a first wallet
       window.ethereum
         .request({ method: "eth_requestAccounts" })
@@ -28,102 +32,195 @@ export default function Home() {
       alert("install metamask extension!!");
     }
   };
-  
+
   // getbalance function for getting a balance in
   // a right format with help of ethers
-  
+
   // Function for getting handling all events
   const accountChangeHandler = (account) => {
     // Setting an address data
-    setWalletAddress(account)
+    setWalletAddress(account);
     // Setting a balance
   };
 
-  const getALL = async () =>{
-    Contract.setProvider(window.ethereum)
+  const getALL = async () => {
+    Contract.setProvider(window.ethereum);
     var contract = new Contract(contractABI, contractAddress);
-    const all = await contract.methods.getProposals().call()
+    const all = await contract.methods.getProposals().call();
     console.log(all);
     console.log(all[0].amount);
-    setAllProposals(all)
-  }
-  const createProposal = async () =>{
-    Contract.setProvider(window.ethereum)
+    setAllProposals(all);
+  };
+  const createProposal = async () => {
+    Contract.setProvider(window.ethereum);
     var contract = new Contract(contractABI, contractAddress);
-    const create = await contract.methods.createProposal(desc, charity, amount).send({ from: walletAddress })
-  }
-  const vote = async (id, choice) =>{
-    Contract.setProvider(window.ethereum)
+    // const create = await contract.methods.createProposal(desc, charity, amount).send({ from: walletAddress })
+  };
+  const vote = async (id, choice) => {
+    Contract.setProvider(window.ethereum);
     var contract = new Contract(contractABI, contractAddress);
-    const vote = await contract.methods.vote(id, choice).send({ from: walletAddress })
-  }
+    const vote = await contract.methods
+      .vote(id, choice)
+      .send({ from: walletAddress });
+  };
 
   useEffect(() => {
-    getALL()
-  }, [])
-  
+    getALL();
+  }, []);
+
   // const abc = await contract.methods.DEFAULT_ADMIN_ROLE().call()
   // const xx1 = await contract.methods.deployer().call()
   // const grantRole1 = await contract.methods.grantRole('0xf4bb84e1768dd286358a736d195a0b228621173f655a73b89bf8b191ed0ba07d', "0x0f2B0d2A580f1CBb5B377916792AbE5471197f4a").send({ from: walletAddress })
   // const grantRole2 = await contract.methods.grantRole('0xf4bb84e1768dd286358a736d195a0b228621173f655a73b89bf8b191ed0ba07d', "0x905819783825B4395D9B1F276556955705378b0f").send({ from: walletAddress })
   // console.log(abc, xx1)
 
-
   return (
     <div className={styles.container}>
-     <h1>{walletAddress}</h1>
-     <button onClick={connectWallet}>Connect Wallet</button><br/>
-     <div className="flex p-2 px-4 gap-2 bg-gray-100 rounded-sm">
-            <input
-              placeholder="Description"
-              className="flex-1 bg-transparent outline-none border-none font-light"
-              type="text"
-              value={desc}
-              onChange={(e)=>setDesc(e.target.value)}  
-            />
-            <span>{desc}</span>
-          </div>
-     <div className="flex p-2 px-4 gap-2 bg-gray-100 rounded-sm">
-            <input
-              placeholder="Charity address"
-              className="flex-1 bg-transparent outline-none border-none font-light"
-              type="text"
-              value={charity}
-              onChange={(e)=>setCharity(e.target.value)}  
-            />
-            <span>{charity}</span>
-          </div>
-     <div className="flex p-2 px-4 gap-2 bg-gray-100 rounded-sm">
-            <input
-              placeholder="Amount"
-              className="flex-1 bg-transparent outline-none border-none font-light"
-              type="Number"
-              value={amount}
-              onChange={(e)=>setAmount(e.target.value)}  
-            />
-            <span>{amount}</span>
-          </div>
-          <button className="bg-red-300 p-2 px-4 mt-2 rounded w-full" onClick={()=>createProposal()}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          alignItems: "center",
+          padding: "8px 16px 8px 16px",
+        }}
+      >
+        {walletAddress && (
+          <button className={styles.btn} onClick={connectWallet}>
+            {walletAddress.substring(0, 4) +
+              "...." +
+              walletAddress.substring(38, walletAddress.length)}
+          </button>
+        )}
+        {!walletAddress && (
+          <button className={styles.btn} onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
+      </div>
+      <form
+        action="#"
+        style={{
+          backgroundColor: "#F6F7FC",
+          width: "50%",
+          padding: "24px",
+        }}
+      >
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: "24px",
+          }}
+        >
+          <input
+            placeholder="Description"
+            style={{
+              width: "100%",
+              padding: "12px",
+              border: "1px solid grey",
+              borderRadius: "10px",
+            }}
+            type="text"
+            value={desc}
+            onChange={(e) => setDesc(e.target.value)}
+          />
+
+          <input
+            placeholder="Charity address"
+            style={{
+              width: "100%",
+              padding: "12px",
+              border: "1px solid grey",
+              borderRadius: "10px",
+            }}
+            type="text"
+            value={charity}
+            onChange={(e) => setCharity(e.target.value)}
+          />
+          <input
+            placeholder="Amount"
+            style={{
+              width: "100%",
+              padding: "12px",
+              border: "1px solid grey",
+              borderRadius: "10px",
+            }}
+            type="Number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+          <button className={styles.btn} onClick={() => createProposal()}>
             Create Proposal
           </button>
-          {allProposals && allProposals.map(e=>
-             ( <div style={{display: "flex", flexDirection: "row",backgroundColor: '#94f', 
-             justifyContent: "center",alignItems: "center", color: "white", padding: '10px'}}>
-              <h3>Description: {e.description}</h3>
-              <button onClick={()=>{vote(e.id, true)}}>Vote for</button>
-              <button onClick={()=>{vote(e.id, false)}}>Vote against</button>
-              <h6>ID: {e.id}</h6>
-              <h6>Amount: {e.amount}</h6>
-              <h6>CharityAddress: {e.charityAddress}</h6>
-              <h6>LivePeriod: {new Date(e.livePeriod*1000).toISOString()}</h6>
-              <h6>Paid: {e.paid}</h6>
-              <h6>PaidBy: {e.paidBy}</h6>
-              <h6>Proposer: {e.proposer}</h6>
-              <h6>VotesAgainst: {e.votesAgainst}</h6>
-              <h6>VotesFor: {e.votesFor}</h6>
-              <h6>VotingPassed: {e.votingPassed}</h6>
-             </div>)
-          )}
+        </div>
+      </form>
+
+      <div
+        style={{
+          display: "flex",
+          columnGap: "24px",
+          rowGap: "20px",
+          flexWrap: "wrap",
+          padding: "24px",
+        }}
+      >
+        {allProposals &&
+          allProposals.map((e) => (
+            <div
+              style={{
+                border: "1px solid grey",
+                padding: "12px",
+                width: "450px",
+                borderRadius: "10px",
+              }}
+            >
+              <h3
+                style={{
+                  fontSize: "24px",
+                }}
+              >
+                Description: {e.description}
+              </h3>
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <button
+                  className={styles.btn}
+                  onClick={() => {
+                    vote(e.id, true);
+                  }}
+                >
+                  Vote for
+                </button>
+                <button
+                  className={styles.btn}
+                  onClick={() => {
+                    vote(e.id, false);
+                  }}
+                >
+                  Vote against
+                </button>
+              </div>
+              <div
+                style={{
+                  fontSize: "15px",
+                  fontWeight: "600",
+                }}
+              >
+                <p>ID: {e.id}</p>
+                <p>Amount: {e.amount}</p>
+                <p>CharityAddress: {e.charityAddress}</p>
+                <p>LivePeriod: {new Date(e.livePeriod * 1000).toISOString()}</p>
+                <p>Paid: {e.paid}</p>
+                <p>PaidBy: {e.paidBy}</p>
+                <p>Proposer: {e.proposer}</p>
+                <p>VotesAgainst: {e.votesAgainst}</p>
+                <p>VotesFor: {e.votesFor}</p>
+                <p>VotingPassed: {e.votingPassed}</p>
+              </div>
+            </div>
+          ))}
+      </div>
     </div>
-  )
+  );
 }
